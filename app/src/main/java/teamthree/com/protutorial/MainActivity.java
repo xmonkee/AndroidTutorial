fef4bfd5ccf900f4f0c49c74c926907c8aeecdb4
Mayank Mandava <mayankmandava@gmail.com>
2015-05-18 18:01:20 -0500

    Added new elements

--- app/src/main/java/teamthree/com/protutorial/MainActivity.java
+++ app/src/main/java/teamthree/com/protutorial/MainActivity.java
@@ -1,15 +1,15 @@
 package teamthree.com.protutorial;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
